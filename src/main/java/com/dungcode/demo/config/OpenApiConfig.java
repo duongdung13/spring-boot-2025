3d795a8e493bcc8262db3304d94dbd204e947c7c
Duong Dung <dungdcth@gmail.com>
2025-04-07 20:31:00 +0700

    Add swagger

---
+++ src/main/java/com/dungcode/demo/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.dungcode.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Documentation")
+                        .version("1.0")
+                        .description("Swagger UI cho Spring Boot"))
+                .addServersItem(new io.swagger.v3.oas.models.servers.Server()
+                        .url("/api")
+                        .description("API Server"));
+    }
+}
